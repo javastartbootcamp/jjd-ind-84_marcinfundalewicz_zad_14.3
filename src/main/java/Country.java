e00562c8589491dafcdee3997d34cd7686e0e7ac
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-02-18 15:51:57 +0100

    Rozwiazanie zadania 14.3

---
+++ src/main/java/Country.java
@@ -0,0 +1,35 @@
+public class Country {
+    private String code;
+    private String name;
+    private long population;
+
+    public Country(String code, String name, long population) {
+        this.code = code;
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
+    String fileName = "countries.csv";
+    File file = new File(fileName);
 
     // nie zmieniaj nic w main
     public static void main(String[] args) {
@@ -10,7 +16,55 @@ public class Main {
     }
 
     void run(Scanner scanner) {
-        // usupełnij metodę
+        try {
+            if (fileExists(file)) {
+                Map<String, Country> countries = fillMapFromFile();
+                String line = reader(scanner);
+                printInfo(line, countries);
+            } else {
+                System.out.println("Brak pliku " + fileName + ".");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    private boolean fileExists(File file) {
+        return file.exists();
+    }
+
+    private static String reader(Scanner scanner) {
+        System.out.println("Proszę podać kod kraju");
+        return scanner.nextLine();
+    }
+
+    private Map<String, Country> fillMapFromFile() throws FileNotFoundException {
+        Map<String, Country> countries = new TreeMap<>();
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                Country country = takeCountryFromText(line);
+                countries.put(country.getCode(), country);
+            }
+            return countries;
+        }
+    }
+
+    private Country takeCountryFromText(String line) {
+        String[] lines = line.split(";");
+        String code = lines[0];
+        String name = lines[1];
+        long population = Long.parseLong(lines[2]);
+        return new Country(code, name, population);
+    }
+
+    private void printInfo(String code, Map<String, Country> countries) {
+        if (countries.containsKey(code)) {
+            Country country = countries.get(code);
+            System.out.println(country.getName() + " (" + country.getCode() + ") ma "
+                    + country.getPopulation() + " ludności.");
+        } else {
+            System.out.println("Kod kraju " + code + " nie został znaleziony.");
+        }
+    }
 }
